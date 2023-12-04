c64d45750939ef4155f43ab55e44f88ec2f8dbed
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2023-12-04 21:45:35 +0000

    updated the eslintrc.js for arrow-function-style error

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,15 @@ module.exports = {
     "linebreak-style": [2, "unix"],
     semi: [2, "always"],
     "no-console": 0,
+    "arrow-body-style": [2, "as-needed"],
   },
   env: {
     es6: true,
     node: true,
     browser: true,
   },
-  parseOptions: {
-    ecmaVersion: 2021
+  parserOptions: {
+    ecmaVersion: 2021,
   },
   extends: "eslint:recommended",
 };
