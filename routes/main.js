2c890663b92caaf7418a25fc36d44c44550cd28f
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2023-12-04 21:47:50 +0000

    add api routes to the main route

--- routes/main.js
+++ routes/main.js
@@ -1,14 +1,26 @@
+// imports
 const router = require("express").Router();
 
+// web routes
 const aboutRoute = require("./about");
 const homeRoute = require("./home");
 const registerRoute = require("./register");
 const loginRoute = require("./login");
 
+// api routes
+const apiLoginRoute = require("./api/login");
+const apiRegisterRoute = require("./api/register");
+
 // exporting routes
+// normal web routes
 router.use("/about", aboutRoute);
 router.use("/", homeRoute);
 router.use("/register", registerRoute);
 router.use("/login", loginRoute);
 
+// api Routes
+router.use("/api/login", apiLoginRoute);
+router.use("/api/register", apiRegisterRoute);
+
+// exports
 module.exports = router;
