e58011c6b9345daba376641c0576cf74abfbed54
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2023-12-10 18:05:59 +0000

    updated the server.js

--- server.js
+++ server.js
@@ -1,53 +1,9 @@
-/* inside the .env file
-
-#-----------------begin------------------------
-
-PORT = 8000
-
-#  Google api
-CLIENT_ID = <YOUR CLIENT_ID>
-CLIENT_SECRET = <YOUR CLIENT_SECRET>
-
-# mongodb URL
-MONGO_URL = <mongodb_url>
-
-#-----------------end of file--------------------
-
-*/
-
-// adding modules
-const express = require("express");
-const path = require("path");
-const env = require("dotenv");
-const ejs = require("ejs");
-const bodyParser = require("body-parser");
-
-// setting up express
-const app = express();
-
-// loading environmental variables
-env.config();
-
-// setting up server variables
-const port = process.env.PORT || 8000;
-
-// setting up view engine
-app.set("view engine", "ejs");
-
-// middleware to parse body of request into json object
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-// Set up static files
-app.use(express.static(path.join(__dirname, "/mvc/view")));
-
-// importing routes
-const mainRoutes = require("./routes/main"); // updated import statement
-// using the routes in our application
-app.use("/", mainRoutes);
+const app = require("./app");
 
 // starting server
 // server listening at port
+const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
   console.log(`Server listening at port: ${port}...`);
 });
