4b1aa525528c95c4bc908fb944b25a70bd2a9975
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2023-12-11 14:04:14 +0000

    Add baseURL to helper_functions

--- test/login_user.test.js
+++ test/login_user.test.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const { login_url, loginUser } = require("./misc/helper_functions");
+const {  baseURL, login_url, loginUser } = require("./misc/helper_functions");
 
 // Mock Axios before importing the module that uses it
 jest.mock("axios");
@@ -30,5 +30,45 @@ describe("Actual Network Tests", () => {
 
     // Assertions for the actual network request
     expect(response).toEqual({ success: true, message: "Login successful" });
+  });
+});
+
+describe('Login Route Test', () => {
+
+
+  test('User login with correct credentials', async () => {
+      // Mock user credentials
+      const userCredentials = {
+          username: 'user',
+          password: 'password'
+      };
+
+      try {
+          const response = await axios.post(`${baseURL}/login`, userCredentials);
+          expect(response.status).toBe(200); // Assuming successful login returns status code 200
+          expect(response.data).toContain('Login Successful!'); // Assuming response contains this string upon successful login
+      } catch (error) {
+        console.log(error);
+          // Handle any errors
+          console.error('Error:', error.response.data);
+      }
+  });
+
+  test('User login with incorrect credentials', async () => {
+      // Mock incorrect user credentials
+      const incorrectCredentials = {
+          username: 'user',
+          password: 'wrongpassword'
+      };
+
+      try {
+          const response = await axios.post(`${baseURL}/login`, incorrectCredentials);
+          expect(response.status).toBe(200); // Assuming failed login also returns status code 200
+          expect(response.data).toContain('Login Failed!'); // Assuming response contains this string upon failed login
+      } catch (error) {
+          console.log(error);
+          // Handle any errors
+          console.error('Error:', error.response);
+      }
   });
 });
