3d7d62e1178deba9f98aa1edeaddc37bf0d85bd3
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2023-12-11 23:31:25 +0000

    Add form validation and passport authentication to register route

--- routes/api/register.js
+++ routes/api/register.js
@@ -1,25 +1,45 @@
 const express = require("express");
+const passport = require("../../mvc/service/passport/passport_main");
 const router = express.Router();
 
+const valid_email = (email) => {
+  let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return email.match(regex);
+};
+
+const valid_password = (password, min_length) => {
+  return ( password.length >= min_length && password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/));
+};
+
+// Middleware for validating the register form
 const validate_register_form = (req, res, next, err) => {
   const { username, password, email } = req.body;
+
+  // check for required fields
   if (!username || !password || !email) {
     return res.status(400).json(new Error("Missing required fields"));
   }
+  // check for valid email 
+  if (!valid_email()) {
+    return res.status(400).json(new Error("Invalid email format"));
+  }
+
+  // check password contain Capital and special characters and length atleast 8 characters.
+  if (valid_password(password, 8)) {
+    return res.status(400).json(
+      new Error(
+        "Password must contain at least one Capital letter, one small letter, one number and one special character"
+      )
+    );
+  }
+  console.log("validation done!");
   next();
 };
 
 // POST route for registering a user
-router.post("/", validate_register_form, (req, res) => {
-  // Assuming req.body contains the user data
-  const userData = req.body;
-
-  // Perform any necessary validation or processing of the user data
-
-  const result = {data : { message: "User registered successfully" }};
-  console.log(result);
-  // Send the user data in JSON format
-  res.json(result);
+// @route: /api/register
+router.post("/", validate_register_form,  passport.authenticate("local"), (req, res) =>{
+  console.log("passport authentication Successfull!");
 });
 
 module.exports = router;
