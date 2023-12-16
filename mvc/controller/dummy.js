5a716119ae806661a399c8a1fe4a0442271e6e09
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2023-12-16 19:46:41 +0000

    updated ejs files and added dummy data.

---
+++ mvc/controller/dummy.js
@@ -0,0 +1,29 @@
+exports.homePage = {
+    title: 'Welcome to our Crypto Market',
+    description: 'Welcome to our platform dedicated to providing comprehensive and up-to-date information about the cryptocurrency market. Whether you’re an enthusiast, investor, or simply curious about digital currencies, we offer a centralized hub to explore, analyze, and stay informed about the latest trends, prices, and developments in the ever-evolving world of cryptocurrencies.',
+  };
+  
+  
+  exports.aboutPage = {
+    title: 'About Us',
+    mission: 'Our mission is to provide the best services by leveraging cutting-edge technology and fostering a culture of innovation. We aim to deliver exceptional products and solutions while prioritizing customer satisfaction. Through our dedication, expertise, and commitment to excellence, we strive to be leaders in our industry and make a positive impact on the global community.',
+    teamMembers: [
+      { name: 'Tom', position: 'CEO' },
+      { name: 'Jerry', position: 'CTO' }
+      // Additional team members can be added here
+    ],
+  };
+  
+  
+  // Contact page object with dummy data
+  exports.contactPage = {
+    title: 'Contact Us',
+    address: '123 Main Street, City, Country',
+    email: 'info@example.com',
+    phone: '+1234567890',
+    socialMedia: {
+      twitter: 'https://twitter.com/example',
+      facebook: 'https://www.facebook.com/example'
+    }
+  };
+
