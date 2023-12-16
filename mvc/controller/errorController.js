33c7bc608dad2816cc3f1192d49d03062780b54d
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2023-12-16 23:02:59 +0000

    Refactor error handling in errorController.js

--- mvc/controller/errorController.js
+++ mvc/controller/errorController.js
@@ -1,11 +1,36 @@
+const devErrors = (res, error) => {
+    res.status(error.statusCode).json({
+        error:{
+            status: error.statusCode,
+            message: error.message,
+            stackTrace: error.stack,
+            error: error
+        }
+    });
+} ;
+
+const prodErrors = (res, error ) => {
+    if(error.isOperational){
+        res.status(error.statusCode).json({
+            error:{
+                status: error.statusCode,
+                message: error.message
+            }
+        });} else {
+            res.status(500).json({
+                status: "error",
+                message: "Something went wrong! Please try again later."
+            });
+        }
+};
+
 module.exports = (error, req, res, next) => {
     error.statusCode = error.statusCode || 500;
     error.message = error.message || "error";
 
-    res.status(error.statusCode).json({
-        error:{
-            status: error.statusCode,
-            message: error.message
-        }
-    });
+    if(process.env.NODE_ENV === "dev"){
+        devErrors(res, error);
+    }else if (process.env.NODE_ENV === "prod"){
+        prodErrors(res, error);
+    }
 };
