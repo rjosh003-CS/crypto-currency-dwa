74fb5c1402c46ec6a74332c9b94a580a762fb20d
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2023-12-19 14:01:47 +0000

    Add error handling for unhandled rejections and uncaught exceptions

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenvExpand = require("dotenv-expand");
 
 dotenvExpand.expand({ parsed: envVars });
 
+
 const createApp = require("./app");
 const { connectToDatabase, createSessionStore } = require("./database");
 
@@ -15,9 +16,27 @@ const app = createApp(store);
 connectToDatabase();
 
 // starting server
-// server listening at port
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+// server listening at port
+const server = app.listen(port, () => {
   console.log(`Server listening at port: ${port}...`);
+});
+
+// handling unhandled promise rejections
+process.on("unhandledRejection", (err)=>{
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION! Shutting down...");
+  server.close(()=>{
+    process.exit(1);
+  });
+});
+
+// handling uncaught exceptions
+process.on("uncaughtException", (err)=>{
+  console.log(err.name, err.message);
+  console.log("UNCAUGHT EXCEPTION! Shutting down...");
+  server.close(()=>{
+    process.exit(1);
+  });
 });
