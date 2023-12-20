9ead291806394a6527874bcb12c5dddb9e4cdf22
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2023-12-20 06:13:57 +0000

    Refactor logout route in routes/api/logout.js

--- routes/api/logout.js
+++ routes/api/logout.js
@@ -1,30 +1,8 @@
 // Assuming you have Express and passport configured
 const router = require("express").Router();
 
-// Logout route
-// router.get("/", (req, res, next)=>{
-//   console.log("inside logout route");
-//   req.logout((err) => {
-    
-//     if (err) {
-//       return next(err);
-//     }
-    
-//     req.session.destroy(); // Destroy the session
-    
-//     // Redirect to Google's logout URL
-//     const googleLogoutURL = "https://accounts.google.com/logout";
-//     // Redirect to Google logout first
-//     res.redirect(googleLogoutURL);
-    
-//     res.redirect("/"); // Redirect after logout
-//   });
-// });
-
 // logout route
-router.get("/", (req, res, next) => {
-  const provider = req.provider;
-
+router.get("/", (req, res) => {
   // Handle normal logout for local strategy
   req.logout((err)=>{
     if (err){ 
@@ -34,18 +12,8 @@ router.get("/", (req, res, next) => {
    req.session.destroy(); // Destroy the session
   });
 
-  if (provider === "google") {
-    // Redirect to Google's logout URL
-    const googleLogoutURL = "https://accounts.google.com/logout";
-    res.redirect(googleLogoutURL);
-  }
-
-  res.redirect("/"); // Redirect to home or login page
-  });
-
-router.get("/google/callback", (req, res) => {
-  // Handle normal logout for local strategy
-  res.redirect("/"); // Redirect to home or login page
+  return res.redirect("/"); // Redirect to home or login page
 });
+
 
 module.exports = router;
