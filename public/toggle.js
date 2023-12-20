c2dd27efd025f7ccdf5e6a689dacfcc47798b1dd
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2023-12-20 05:06:42 +0000

    Refactor toggle.js to store and apply mode on page load

--- public/toggle.js
+++ public/toggle.js
@@ -1,23 +1,32 @@
 // JavaScript for toggling light and dark modes
 const toggleMode = () => {
-    const body = document.body;
-    body.classList.toggle('dark-mode');
-    body.classList.toggle('light-mode');
-  };
-  
+  const body = document.body;
+  body.classList.toggle('dark-mode');
+  body.classList.toggle('light-mode');
 
-// Toggle sidebar
-document.addEventListener('DOMContentLoaded', function() {
+  // Store the current mode in localStorage
+  const currentMode = body.classList.contains('dark-mode') ? 'dark' : 'light';
+  localStorage.setItem('mode', currentMode);
+};
+
+// Apply stored mode on page load
+document.addEventListener('DOMContentLoaded', function () {
+  const savedMode = localStorage.getItem('mode');
+
+  if (savedMode === 'dark') {
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+  }
+
   const sidebarToggle = document.getElementById('sidebarToggle');
   const sidebar = document.querySelector('.sidebar');
-  const sidebarToggleIcon = document.querySelector('#sidebarToggle .fa-burger');
 
-  sidebarToggle.addEventListener('click', function(event) {
+  sidebarToggle.addEventListener('click', function (event) {
     sidebar.classList.toggle('show');
   });
 });
 
-  // Example: Toggle mode when a button is clicked
-  const modeToggleBtn = document.getElementById('modeToggleBtn');
-  modeToggleBtn.addEventListener('click', toggleMode);
-  
+// Example: Toggle mode when a button is clicked
+const modeToggleBtn = document.getElementById('modeToggleBtn');
+modeToggleBtn.addEventListener('click', toggleMode);
