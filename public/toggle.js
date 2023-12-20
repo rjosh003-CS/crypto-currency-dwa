7dd74467a7e67ce09b62bc70a8dba979de14c618
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2023-12-20 05:03:50 +0000

    Add sidebar toggle functionality

--- public/toggle.js
+++ public/toggle.js
@@ -5,6 +5,18 @@ const toggleMode = () => {
     body.classList.toggle('light-mode');
   };
   
+
+// Toggle sidebar
+document.addEventListener('DOMContentLoaded', function() {
+  const sidebarToggle = document.getElementById('sidebarToggle');
+  const sidebar = document.querySelector('.sidebar');
+  const sidebarToggleIcon = document.querySelector('#sidebarToggle .fa-burger');
+
+  sidebarToggle.addEventListener('click', function(event) {
+    sidebar.classList.toggle('show');
+  });
+});
+
   // Example: Toggle mode when a button is clicked
   const modeToggleBtn = document.getElementById('modeToggleBtn');
   modeToggleBtn.addEventListener('click', toggleMode);
