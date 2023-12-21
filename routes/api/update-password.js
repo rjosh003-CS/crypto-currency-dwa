f018d3d58fdcdf1939337215a7fc3bf4d50d6592
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2023-12-21 06:11:23 +0000

    Refactor password update route

--- routes/api/update-password.js
+++ routes/api/update-password.js
@@ -2,44 +2,47 @@ const router = require("express").Router();
 const User = require("../../mvc/models/userSchema");
 const { encrypt, compare } = require("../../mvc/controller/helperController");
 
-
 // Backend route handling the password update
-// @route /profile/update-password
-router.route("/")
-.post(  async (req, res) => {
-    const { oldPassword, newPassword, confirmPassword, email } = req.body;
+// @route /api/profile/update-password
+router.route("/").post(async (req, res) => {
+  const { oldPassword, newPassword, confirmPassword, email } = req.body;
+  console.log("->", req.body);
 
-    // Check if newPassword and confirmPassword match
-    if (newPassword !== confirmPassword) {
-        return res.status(400).json({ message: "New passwords don't match" });
+  // Check if newPassword and confirmPassword match
+  if (newPassword !== confirmPassword) {
+    return res.status(400).json({ message: "New passwords don't match" });
+  }
+
+  // Check if the newPassword and oldPassword are not the same
+  if (newPassword === oldPassword) {
+    return res.status(400).json({ message: "New passwords and old password are the same" });
+  }
+
+  try {
+    // Fetch the user from the database by email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(500).json({ message: "User not found" });
     }
 
-    // Check if the newPassword and oldPassword are not same
-    if (newPassword === oldPassword) {
-        return res.status(400).json({ message: "New passwords and old password are same" });
+    // Verify old password
+    const isMatch = await compare(oldPassword, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Old password is incorrect" });
     }
-    
-    // Fetch the user from the database by userId
-    User.findBy({email: email}, async (err, user) => {
-        if (err || !user) {
-            return res.status(500).json({ message: "User not found" });
-        }
 
-        // Verify old password
-        const isMatch = await compare(oldPassword, user.password);
-        if (!isMatch) {
-            return res.status(401).json({ message: "Old password is incorrect" });
-        }
+    // Update the password with the new one
+    const hashedPassword = encrypt(newPassword);
+    user.password = hashedPassword;
+    await user.save();
 
-        // Update the password with the new one
-        user.password = newPassword;
-        user.save((err) => {
-            if (err) {
-                return res.status(500).json({ message: "Failed to update password" });
-            }
-            return res.status(200).json({ message: "Password updated successfully" });
-        });
-    });
+    return res.status(200).json({ message: "Password updated successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Failed to update password" });
+  }
 });
 
 module.exports = router;
