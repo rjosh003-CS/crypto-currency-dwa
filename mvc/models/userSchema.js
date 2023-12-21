d2a998bdf9fe016fc3b1ace7e7ed59a1e6904fc9
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2023-12-21 01:38:19 +0000

    Refactor userSchema.js to simplify role and subscription fields

--- mvc/models/userSchema.js
+++ mvc/models/userSchema.js
@@ -1,35 +1,6 @@
 // importing the required module
 const mongoose = require("mongoose");
 
-// defining the schema for role
-const roleSchema = new mongoose.Schema({
- role: {
-    type: String,
-    enum: ["user", "admin"], // enumeration of valid values for role
-    default: "user" // default value for role
- },
- role_verified: {
-    type: Boolean,
-    default: false // default value for role_verified
- }
-});
-
-// defining the schema for subscription
-const subscriptionSchema = mongoose.Schema({
- subscribtion_type: {
-    type: String,
-    enum: ["free", "premium"], // enumeration of valid values for subscribtion_type
-    default: "free" // default value for subscribtion_type
- },
- subscribtion_start_date: {
-    type: Date
- },
- subscribtion_end_date: {
-    type: Date
- }
-});
-
-
 // defining the schema for user
 const userSchema = new mongoose.Schema({
   displayname: {
@@ -91,9 +62,11 @@ const userSchema = new mongoose.Schema({
     unique: true, // email must be unique
   },
 
-  role: roleSchema, // defining the role schema within the user schema
-
-  subscription: subscriptionSchema, // defining the subscription schema within the user schema
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
 
   google_id: {
     type: String,
@@ -173,9 +146,6 @@ userSchema.methods.createResetPassword = async function (){
 
 // creating a mongoose model for user with the defined schema
 const User = mongoose.model("User", userSchema);
-
-
-// const Role = mongoose.model("Roles", userSchema);
 
 // exporting the model
 module.exports = User;
