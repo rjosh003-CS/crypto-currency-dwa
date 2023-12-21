06450be4ec2da24a8280c29023c412b63622e94c
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2023-12-21 06:10:59 +0000

    Add user email validation and update profile functionality

--- routes/profile.js
+++ routes/profile.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { profile } = require("../mvc/controller/routeController");
+const User = require("../mvc/models/userSchema");
 
 // @route: /profile 
 router
@@ -17,21 +18,70 @@ router
     res.status(200).json(data);
   });
 
+
 // @route: /profile/edit
-router
-.route("/edit")
-.post((req, res) => {
-  console.log(req.body);
-  // todo!
-  const data = Object.assign(
-    {},
-    {
-      title: "Profile Page",
-      currentYear: new Date().getFullYear(),
-      user: req.user
+router.route("/edit").post(async (req, res) => {
+  try {
+    const userEmail = req.body.userEmail;
+    const newEmail = req.body.email; // Assuming the new email to be updated
+
+    // Check if the new email is different from the current user's email
+    if (newEmail !== userEmail) {
+      const emailExists = await User.findOne({ email: newEmail });
+      if (emailExists) {
+
+        const status = "Email not available";
+        const message = "Email already exists for another user"
+
+        const data = {
+          status: error,
+          message: message
+        }
+        const newData = JSON.stringify(data);
+        console.log(newData);
+        req.flash("errors", newData)
+        return;
+        // return res.status(400).json({ error: 'Email already exists for another user.' });
+      }
     }
-  );
-  res.status(200).json(data);
+
+    // Filter out empty or undefined fields from the request body
+    const updatedData = Object.fromEntries(
+      Object.entries(req.body).filter(([key, value]) => value !== undefined && value !== '')
+    );
+
+    delete updatedData.userEmail;
+
+    const updatedUser = await User.findOneAndUpdate(
+      { email: userEmail },
+      { $set: updatedData },
+      { new: true }
+    );
+
+    const message = "Profile updated successfully";
+    req.flash("success", message);
+
+    return res.redirect("/profile");
+    // res.status(200).json(updatedUser);
+  } catch (err) {
+    console.error(err);
+
+    const status = "Fail";
+    const message = "Internal Server Error"
+
+    const data = {
+      status: error,
+      message: message
+    }
+    const newData = JSON.stringify(data);
+    console.log(newData);
+    req.flash("errors", newData)
+    return;
+    // res.status(500).json({ error: "Internal Server Error" });
+  }
 });
+
+
+
 
 module.exports = router;
