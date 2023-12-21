1c2c753a18abe91e5b77e49098bdb55a7e4fa5b6
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2023-12-21 23:26:51 +0000

    Add user route and update error handling

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,10 @@
 // imports
 const colour = require("../color_code");
 const router = require("express").Router();
-const { isAuthenticated, isNotAuthenticated, forgotPassword, passwordReset}  = require("./helper_function");
+const { isAuthenticated, isNotAuthenticated, restrict, forgotPassword, passwordReset}  = require("./helper_function");
 const globalErroHandler = require("../mvc/controller/errorController");
 const CustomError = require ("../utils/CustomError");
+const { page404 } = require("../mvc/controller/routeController");
 
 // web routes
 const aboutRoute = require("./about");
@@ -15,6 +16,7 @@ const financeRoute = require("./finance");
 const profileRoute = require("./profile");
 const dashboardRoute = require("./dashboard");
 const passwordUpdateRoute = require("./password-update");
+const userRoute = require("./user");
 
 // api routes
 const apiRoleRoute = require("./api/role");
@@ -22,8 +24,9 @@ const apiLoginRoute = require("./api/login");
 const apiRegisterRoute = require("./api/register");
 const apiLogoutRoute = require("./api/logout");
 const apiCurrecyRoutes = require ("./api/currency");
-// const apiPasswordChange = require("./api/update-password");
 
+// todo! kept for the later development
+// const apiPasswordChange = require("./api/update-password");
 // const apiGoogleRegisterRoute = require("./api/registerGoogle");
 
 // Middleware for logging requests
@@ -31,6 +34,7 @@ router.use((req, res, next) => {
     console.log(`${colour.blue}@method: ${colour.yellow}${req.method} \t ${colour.blue}@url: ${colour.yellow}${ req.originalUrl} ${colour.reset}`);
     next();
 });
+
 
 // exporting routes
 // normal web routes
@@ -44,19 +48,12 @@ router.use("/profile", isAuthenticated, profileRoute);
 router.use("/dashboard", isAuthenticated, dashboardRoute);
 router.use("/profile/password-update", isAuthenticated, passwordUpdateRoute);
 
+// admin routes
+router.use("/admin/user", isAuthenticated, userRoute);
+
 // API Routes
 router.use("/register", isNotAuthenticated, registerRoute);
 router.use("/login", isNotAuthenticated, loginRoute);
-
-// Routes for success after login
-router.get("/success", (req, res) => {
-    res.status(200).render("200page",  {message: "Login Successful!"});
-});
-
-// Routes for failure after login
-router.get("/failure", (req, res) => {
-    res.status(401).send( "401page" , {message:"Login Failed!"});
-});
 
 // api Routes
 // router.use("/api/profile/update-password", isAuthenticated, apiPasswordChange); 
@@ -74,9 +71,20 @@ router.route("/api/v1/users/resetPassword/:token").patch (passwordReset);
 router.use("/api", isAuthenticated, apiCurrecyRoutes);
 router.use("/api", isAuthenticated, apiRoleRoute);
 
+// Routes for success after login
+router.get("/success", (req, res) => {
+    res.status(200).render("200page",  {message: "Login Successful!"});
+});
+
+// Routes for failure after login
+router.get("/failure", (req, res) => {
+    res.status(401).send( "401page" , {message:"Login Failed!"});
+});
 
 // 404 route
-router.all("*", (req, res, next)=> {
+router.route("*")
+.get( page404)
+.post( (req, res, next)=> {
     const error = new CustomError (`Can't find '${req.originalUrl}' on this server!`, 404 );
     // sending error to global error handler.
     next(error);
