b8272ebbf172526e5060dc23036d32cec4e04a06
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2023-12-21 01:02:04 +0000

    Update profile route and add edit route

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { profile } = require("../mvc/controller/routeController");
 
-// profile route
+// @route: /profile 
 router
   .route("/")
   .get(profile)
@@ -11,10 +11,27 @@ router
       {
         title: "Profile Page",
         currentYear: new Date().getFullYear(),
-        user: req.user,
+        user: req.user
       }
     );
     res.status(200).json(data);
   });
+
+// @route: /profile/edit
+router
+.route("/edit")
+.post((req, res) => {
+  console.log(req.body);
+  // todo!
+  const data = Object.assign(
+    {},
+    {
+      title: "Profile Page",
+      currentYear: new Date().getFullYear(),
+      user: req.user
+    }
+  );
+  res.status(200).json(data);
+});
 
 module.exports = router;
