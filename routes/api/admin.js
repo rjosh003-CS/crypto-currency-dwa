a6d1b537babc837f4b6cec3578dc2e8f09b69e06
rjosh003 <rjosh003@campus.goldsmiths.ac.uk>
2023-12-22 04:13:00 +0000

    Update admin API routes for autocomplete and search functionality

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -1,20 +1,22 @@
 const router = require("express").Router();
-const User = require("../mvc/models/userSchema"); // Update with your actual model
+const User = require("../../mvc/models/userSchema"); // Update with your actual model
 
 // API route for auto-completion
-// @route: /admin/api/v1/autocomplete
+// @route: /api/v1/admin/autocomplete?term=${searchTerm}
 router.get("/autocomplete", async (req, res) => {
-  const { param } = req.query;
-
+  const { term } = req.query;
+  console.log(term);
+  
   try {
     const users = await User.find({
       $or: [
-        { name: { $regex: param, $options: "i" } },
-        { username: { $regex: param, $options: "i" } },
-        { email: { $regex: param, $options: "i" } },
+        { displayname: { $regex: term, $options: "i" } },
+        { username: { $regex: term, $options: "i" } },
+        { email: { $regex: term, $options: "i" } },
       ],
     }).limit(10);
-
+    
+    console.log(users);
     return res.json(users);
   } catch (err) {
     return res.status(500).json({ status: "fail", message: "Server Error" });
@@ -22,31 +24,38 @@ router.get("/autocomplete", async (req, res) => {
 });
 
 // API route for fetching paginated search results
-// @route: /admin/api/v1/search
+//  @route: /api/v1/admin/search?param=query&page=${pageNumber}
 router.get("/search", async (req, res) => {
   const { param, page = 1, limit = 10 } = req.query;
+  console.log(param);
 
   try {
     const regexParam = new RegExp(param, "i");
     const query = {
       $or: [
-        { name: { $regex: regexParam } },
+        { displayname: { $regex: regexParam } },
         { username: { $regex: regexParam } },
         { email: { $regex: regexParam } },
       ],
     };
-
+    
     const options = {
       page: parseInt(page),
       limit: parseInt(limit),
     };
-
+    
     const users = await User.paginate(query, options);
-
-    return res.json(users);
+    
+    console.log(users.docs.length);
+    // Returning only relevant user accounts that match the search query
+    return res.json(users); // Assuming 'docs' contains the array of user accounts
   } catch (err) {
     return res.status(500).json({ status: "fail", message: "Server Error" });
   }
 });
 
+
 module.exports = router;
+
+// @route: /admin/api/v1/autocomplete
+// @route: /admin/api/v1/search
